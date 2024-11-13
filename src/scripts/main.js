dfe68c2bd20d48125f061e80f54a5c17833898af
Dominik Dudek <dom-dud774@o2.pl>
2024-11-14 00:05:57 +0100

    Task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write your code here
+
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  const row = table.rows[i];
+  const sourceCell = row.cells[1];
+  const newCell = row.insertCell(row.cells.length - 1);
+
+  newCell.textContent = sourceCell.textContent;
+
+  if (i === 0 || i === table.rows.length - 1) {
+    const thElement = document.createElement('th');
+
+    thElement.textContent = sourceCell.textContent;
+    row.replaceChild(thElement, newCell);
+  }
+}
