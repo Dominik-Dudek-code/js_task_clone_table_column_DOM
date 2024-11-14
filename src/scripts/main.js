d34902b89c550ab231d13551906d4de6127181cb
Dominik Dudek <dom-dud774@o2.pl>
2024-11-14 17:01:10 +0100

    Changed replace cell method, simplify code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,15 +6,8 @@ const table = document.querySelector('table');
 
 for (let i = 0; i < table.rows.length; i++) {
   const row = table.rows[i];
-  const sourceCell = row.cells[1];
+  const sourceCell = row.cells[1].cloneNode(true);
   const newCell = row.insertCell(row.cells.length - 1);
 
-  newCell.textContent = sourceCell.textContent;
-
-  if (i === 0 || i === table.rows.length - 1) {
-    const thElement = document.createElement('th');
-
-    thElement.textContent = sourceCell.textContent;
-    row.replaceChild(thElement, newCell);
-  }
+  row.replaceChild(sourceCell, newCell);
 }
