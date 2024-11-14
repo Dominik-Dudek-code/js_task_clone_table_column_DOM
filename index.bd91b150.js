f5630be32265859df4e24f9a136b033b143a446e
Dominik Dudek <dom-dud774@o2.pl>
2024-11-14 17:01:34 +0100

    make build

---
+++ index.bd91b150.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table"),l=0;l<e.rows.length;l++){var r=e.rows[l],o=r.cells[1].cloneNode(!0),c=r.insertCell(r.cells.length-1);r.replaceChild(o,c)}
+//# sourceMappingURL=index.bd91b150.js.map
